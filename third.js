89810b9a4d6284aba3863d1e3e9c30ab78d5c80b
braau77 <30877997+braau77@users.noreply.github.com>
2018-05-04 13:28:07 -1000

    Update third.js

--- third.js
+++ third.js
@@ -2,7 +2,7 @@ function showEverything(){
   var a = localStorage.getItem('settingName');
   document.getElementById('showName').innerHTML = 'Hello ' + a;
   var b = localStorage.getItem('settingHobby');
-  var rand = b[Math.floor(Math.random() * b.length)];
-  document.getElementById('showHobby').innerHTML = rand;
+  var rand = parseInt(b[Math.floor(Math.random() * b.length)]);
+  document.getElementById('showHobby').innerHTML = "Let's do your favorite activity, " + rand;
   
 }
